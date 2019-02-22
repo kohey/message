ef11bf925178a1b2684c44dce37055e2a169faab
kohey <k_kawamura@kiizan-kiizan.co.jp>
2019-02-22 16:14:20 +0900

    新規作成、編集にバリデーションを加えて、views/commonsに配置して@includeすることで共通部分として扱いました

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.app')
 @section('content')
+
+<!-- @if (count($errors) > 0)
+<ul class='alert alert-danger' role='alert'>
+    @foreach($errors->all() as $error)
+    <li class="ml-4">{{$error}}</li>
+    @endforeach
+</ul>
+@endif -->
 <div class="row">
     <div class="col-6">
         {!! Form::model($message,['route'=>'messages.store'])!!}
--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app');
+@extends('layouts.app')
 @section('content')
 
 <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
